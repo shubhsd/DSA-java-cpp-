7efca6aa19fba7b700a48336436a113d1755482c
Shubham <shubhsd@users.noreply.github.com>
2022-03-10 20:44:24 +0530

    evenOrOdd , isPrime, fibonacci number

--- DAY-1.1(PC)/evenOrOdd.java
+++ DAY-1.1(PC)/evenOrOdd.java
@@ -4,7 +4,7 @@ public class evenOrOdd {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int num = scn.nextInt();
-
+        
         int p = 1;
         while (p <= num) {
             int rem = p % 2;
