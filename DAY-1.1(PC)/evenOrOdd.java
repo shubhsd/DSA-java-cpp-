2465db3c820ca83ab105a24ce16e2ee5030308e0
Shubham <shubhsd@users.noreply.github.com>
2022-03-10 18:49:42 +0530

    even or odd list

---
+++ DAY-1.1(PC)/evenOrOdd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class evenOrOdd {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+
+        int p = 1;
+        while (p <= num) {
+            int rem = p % 2;
+
+            if (rem == 0) {
+                System.out.println( p + " is Even");
+            } else {
+                System.out.println(p + " is Odd");
+            }
+            p++;
+        }
+        System.out.println("Out of the loop");
+    }
+}
