fa5e3292244e278cc3efdbcd35b8b836372aa7b9
Shubham <shubhsd@users.noreply.github.com>
2022-03-11 12:39:30 +0530

    PRINT DIGITS

---
+++ DAY-2.3.4(PC)/printDigits.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class printDigits {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int num = scn.nextInt();
+
+        // Finding divisor
+
+        int div = 1;
+        int temp = num;
+        while (temp >= 10) {
+            temp = temp / 10;
+            div *= 10;
+        }
+        while (div >= 1) {
+            int q = num / div;
+            int r = num % div;
+
+            System.out.println(q);
+            num = r;
+            div = div / 10;
+        }
+    }
+}
