9702050895d9c880cc962a80fdc1d2106b5b925b
Shubham <shubhsd@users.noreply.github.com>
2022-03-23 08:04:38 +0530

    pattern 10

---
+++ DAY-8(PC)/pattern10.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class pattern10 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int n_out_space = n / 2;
+        int n_inner_space = -1;
+
+        for (int i = 1; i <= n; i++) {
+            // System.out.println(n_out_space + ", " + n_inner_space);
+
+            for (int j = 1; j <= n_out_space; j++) {
+                System.out.print("\t");
+            }
+
+            System.out.print("*\t");
+
+            for (int j = 1; j <= n_inner_space; j++) {
+                System.out.print("\t");
+            }
+
+            if (i > 1 && i < n) {
+                System.out.print("*\t");
+            }
+
+            if (i <= n / 2) {
+                n_out_space--;
+                n_inner_space += 2;
+            } else {
+                n_out_space++;
+                n_inner_space -= 2;
+            }
+
+            System.out.println();
+        }
+    }
+}
